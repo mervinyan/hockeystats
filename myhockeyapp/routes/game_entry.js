726ef42889a8cc49ab4fc23400e5f8ea7ac87571
mervinyan <mervin.yan@gmail.com>
2016-05-27 17:05:00 -0600

    added two columns: event #, created date

--- myhockeyapp/routes/game_entry.js
+++ myhockeyapp/routes/game_entry.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var ges = require('ges-client');
 var uuid = require('node-uuid');
 
+var moment = require('moment');
+
 router.get('/', function (req, res, next) {
     res.render('game_list.pug', { title: 'Games', 'games': [] });
 });
@@ -24,7 +26,7 @@ router.get('/fetch', function (req, res, next) {
 });
 
 router.post('/add', function (req, res, next) {
-    append_event(req, res,
+    process(req, res,
         function (req) {
             req.checkBody('date', 'Date is required').notEmpty().isDate();
             req.checkBody('time', 'Time is required').notEmpty();
@@ -64,7 +66,7 @@ router.get('/:streamid', function (req, res, next) {
             if (gameover == false && event.EventType == 'GameEnded') {
                 gameover = true;
             }
-            game_events[i] = { type: event.EventType, json: JSON.parse(bin2String(event.Data.toJSON().data)) };
+            game_events[i] = { number: event.EventNumber, type: event.EventType, createdDate: event.Created, json: JSON.parse(bin2String(event.Data.toJSON().data)) };
         }
         res.render('game_entry.pug', { title: 'Game Events', 'stream_id': streamid, 'game_events': game_events, 'gamestart': gamestart, 'gameover': gameover });
     });
@@ -76,7 +78,7 @@ router.get('/:streamid/timeline', function (req, res, next) {
         var game_events = [];
         for (var i = 0; i < readResult.Events.length; i++) {
             var event = readResult.Events[i].Event;
-            game_events[i] = { type: event.EventType, json: JSON.parse(bin2String(event.Data.toJSON().data)) };
+            game_events[i] = { number: event.EventNumber, type: event.EventType, createdDate: event.Created, json: JSON.parse(bin2String(event.Data.toJSON().data)) };
         }
         res.render('game_timeline.pug', { title: 'Game Timeline', 'stream_id': streamid, 'game_events': game_events });
     });
@@ -90,20 +92,21 @@ router.get('/fetchevents/:streamid', function (req, res, next) {
         var gamestart = false;
         for (var i = 0; i < readResult.Events.length; i++) {
             var event = readResult.Events[i].Event;
+            console.log(event);
             if (gamestart == false && event.EventType == 'GameStarted') {
                 gamestart = true;
             }
             if (gameover == false && event.EventType == 'GameEnded') {
                 gameover = true;
             }
-            game_events[i] = { type: event.EventType, json: JSON.parse(bin2String(event.Data.toJSON().data)) };
+            game_events[i] = { number: event.EventNumber, type: event.EventType, createdDate: convert_to_date(event.Created), json: JSON.parse(bin2String(event.Data.toJSON().data)) };
         }
         res.json({ 'data': game_events, 'stream_id': streamid, 'gamestart': gamestart, 'gameover': gameover });
     });
 });
 
 router.post('/gamestart', function (req, res, next) {
-    append_event(req, res,
+    process(req, res,
         function (req) {
             req.checkBody('date', 'Date is required').notEmpty().isDate();
             req.checkBody('time', 'Time is required').notEmpty();
@@ -123,27 +126,27 @@ router.post('/gamestart', function (req, res, next) {
 });
 
 router.post('/homescore', function (req, res, next) {
-    append_event(req, res, validateScoreRequest, extractScoreEventData, 'GoalScored');
+    process(req, res, validateScoreRequest, extractScoreEventData, 'GoalScored');
 
 });
 
 
 router.post('/homepenalty', function (req, res, next) {
-    append_event(req, res, validatePenaltyRequest, extractPenaltyEventData, 'PenaltyTaken');
+    process(req, res, validatePenaltyRequest, extractPenaltyEventData, 'PenaltyTaken');
 });
 
 
 router.post('/guestscore', function (req, res, next) {
-    append_event(req, res, validateScoreRequest, extractScoreEventData, 'OpponentGoalScored');
+    process(req, res, validateScoreRequest, extractScoreEventData, 'OpponentGoalScored');
 });
 
 
 router.post('/guestpenalty', function (req, res, next) {
-    append_event(req, res, validatePenaltyRequest, extractPenaltyEventData, 'OpponentPenaltyTaken');
+    process(req, res, validatePenaltyRequest, extractPenaltyEventData, 'OpponentPenaltyTaken');
 });
 
 router.post('/gameend', function (req, res, next) {
-    append_event(req, res,
+    process(req, res,
         function (req) {
             req.checkBody('date', 'Date is required').notEmpty().isDate();
             req.checkBody('time', 'Time is required').notEmpty();
@@ -160,7 +163,7 @@ router.post('/gameend', function (req, res, next) {
         'GameEnded');
 });
 
-function append_event(req, res, requestValidator, eventDataExtractor, eventType) {
+function process(req, res, requestValidator, eventDataExtractor, eventType) {
     var errors = requestValidator(req);
     if (errors) {
         res.json({ flash: { type: 'alert-danger', messages: errors } });
@@ -170,42 +173,47 @@ function append_event(req, res, requestValidator, eventDataExtractor, eventType)
             stream = 'game-' + uuid.v4();
         }
         var eventData = eventDataExtractor(req);
-        var connection = ges({ host: '127.0.0.1' });
-        connection.on('connect', function () {
-            console.log('connecting to geteventstore...');
-            connection.readStreamEventsBackward(stream, { start: -1, count: 1, resolveLinkTos: true }, function (err, readResult) {
-                if (err) return console.log('Ooops!', err);
-                console.log(readResult);
-                var expectedVersion = readResult.LastEventNumber;
-                console.log(expectedVersion);
-                var appendData = {
-                    expectedVersion: expectedVersion,
-                    events: [
-                        {
-                            EventId: uuid.v4(),
-                            Type: eventType,
-                            Data: new Buffer(JSON.stringify(eventData)),
-                            IsJson: true
-                        }
-                    ]
-                };
-                connection.appendToStream(stream, appendData, function (err, appendResult) {
-                    if (err) return console.log('Oops!', err);
-                    res.json(appendResult);
-                });
-
-            });
-        });
-
+        append_event_to_stream(stream, eventType, eventData, function (appendResult) {
+            res.json(appendResult);
+        })
     }
 }
 
-function fetch_events_backwards(stream, start, count, resolveLinkTos, resultProcessor) {
+function append_event_to_stream(stream, eventType, eventData, callback) {
     var connection = ges({ host: '127.0.0.1' });
     connection.on('connect', function () {
         console.log('connecting to geteventstore...');
-        connection.readStreamEventsBackward(stream, { start: start, count: count, resolveLinkTos: count }, function (err, readResult) {
+        connection.readStreamEventsBackward(stream, { start: -1, count: 1, resolveLinkTos: true }, function (err, readResult) {
             if (err) return console.log('Ooops!', err);
+            var expectedVersion = readResult.LastEventNumber;
+            var appendData = {
+                expectedVersion: expectedVersion,
+                events: [
+                    {
+                        EventId: uuid.v4(),
+                        Type: eventType,
+                        Data: new Buffer(JSON.stringify(eventData)),
+                        IsJson: true
+                    }
+                ]
+            };
+            connection.appendToStream(stream, appendData, function (err, appendResult) {
+                if (err) return console.log('Oops!', err);
+                callback(appendResult);
+            });
+
+        });
+    });
+}
+
+function fetch_events_backwards(stream, start, count, resolveLinkTos, resultProcessor) {
+    console.log(resolveLinkTos);
+    var connection = ges({ host: '127.0.0.1' });
+    connection.on('connect', function () {
+        console.log('connecting to geteventstore...');
+        connection.readStreamEventsBackward(stream, { start: start, count: count, resolveLinkTos: resolveLinkTos }, function (err, readResult) {
+            if (err) return console.log('Ooops!', err);
+            console.log(readResult);
             resultProcessor(readResult);
         });
     });
@@ -275,6 +283,13 @@ function sort_by_date_time(a, b) {
 
 function left_pad_zero(time) {
     return "00000".substring(0, 5 - time.length) + time
+}
+
+function convert_to_date(timestamp) {
+    var t = new moment(timestamp);
+    var formatted = t.format("dd.mm.yyyy hh:MM:ss");
+    console.log(formatted);
+    return formatted;
 }
 
 module.exports = router;
